90e812298515d3167bdec93ccec726f3136508a6
Shreydalal <shreydalal3188@gmail.com>
2025-01-24 15:14:56 +0530

    For deploy

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/shreydalal.github.io/',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
